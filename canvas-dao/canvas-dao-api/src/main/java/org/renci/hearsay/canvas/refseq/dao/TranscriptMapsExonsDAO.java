b136891db19f8837265cfb219282b99ac99d62f9
Jason Reilly <jdr0887@renci.org>
2014-06-23 15:32:46 -0400

    adding findByGenomeRefIdAndRefSeqVersionAndAccession...for testing

--- canvas-dao/canvas-dao-api/src/main/java/org/renci/hearsay/canvas/refseq/dao/TranscriptMapsExonsDAO.java
+++ canvas-dao/canvas-dao-api/src/main/java/org/renci/hearsay/canvas/refseq/dao/TranscriptMapsExonsDAO.java
@@ -11,6 +11,9 @@ public interface TranscriptMapsExonsDAO extends BaseDAO<TranscriptMapsExons, Int
     public List<TranscriptMapsExons> findByGenomeRefIdAndRefSeqVersion(Integer genomeRefId, String refSeqVersion)
             throws HearsayDAOException;
 
+    public List<TranscriptMapsExons> findByGenomeRefIdAndRefSeqVersionAndAccession(Integer genomeRefId,
+            String refSeqVersion, String accession) throws HearsayDAOException;
+
     public List<TranscriptMapsExons> findByTranscriptMapsId(Integer id) throws HearsayDAOException;
 
     public List<TranscriptMapsExons> findByTranscriptVersionIdAndTranscriptMapsMapCount(String versionId,
