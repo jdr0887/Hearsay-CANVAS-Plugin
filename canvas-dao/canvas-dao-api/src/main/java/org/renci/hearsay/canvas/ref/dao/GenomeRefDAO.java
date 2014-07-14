33600cb82a194653636ec40c92816e79a45bb48f
Jason Reilly <jdr0887@renci.org>
2014-07-14 16:23:38 -0400

    initial commit

---
+++ canvas-dao/canvas-dao-api/src/main/java/org/renci/hearsay/canvas/ref/dao/GenomeRefDAO.java
@@ -0,0 +1,8 @@
+package org.renci.hearsay.canvas.ref.dao;
+
+import org.renci.hearsay.canvas.dao.BaseDAO;
+import org.renci.hearsay.canvas.ref.dao.model.GenomeRef;
+
+public interface GenomeRefDAO extends BaseDAO<GenomeRef, Long> {
+
+}
