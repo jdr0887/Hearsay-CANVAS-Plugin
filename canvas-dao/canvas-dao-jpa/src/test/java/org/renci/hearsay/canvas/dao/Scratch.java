1bd8c8b9695ca6aaf8ced6b1d1fa8fd8d9e3eb9f
Jason Reilly <jdr0887@gmail.com>
2014-11-20 13:13:03 -0500

    moving features into transcriptrefseq creation block

--- canvas-dao/canvas-dao-jpa/src/test/java/org/renci/hearsay/canvas/dao/Scratch.java
+++ canvas-dao/canvas-dao-jpa/src/test/java/org/renci/hearsay/canvas/dao/Scratch.java
@@ -244,6 +244,30 @@ public class Scratch {
                             Long id = hearsayDAOBean.getTranscriptRefSeqDAO().save(transcriptRefSeq);
                             hearsayEM.getTransaction().commit();
                             transcriptRefSeq.setId(id);
+
+                            List<Feature> canvasFeatures = canvasDAOBean.getFeatureDAO()
+                                    .findByRefSeqVersionAndTranscriptId(refSeqVersion, key.getVersionId());
+                            if (canvasFeatures != null && !canvasFeatures.isEmpty()) {
+                                for (Feature canvasFeature : canvasFeatures) {
+                                    RegionGroup regionGroup = canvasFeature.getRegionGroup();
+                                    if (regionGroup != null) {
+                                        Set<RegionGroupRegion> regionGroupRegions = regionGroup.getRegionGroupRegions();
+                                        if (regionGroupRegions != null && !regionGroupRegions.isEmpty()) {
+                                            RegionGroupRegion[] regionGroupRegionArray = regionGroupRegions
+                                                    .toArray(new RegionGroupRegion[regionGroupRegions.size()]);
+                                            org.renci.hearsay.dao.model.Feature hearsayFeature = new org.renci.hearsay.dao.model.Feature();
+                                            hearsayFeature.setNote(canvasFeature.getNote());
+                                            hearsayFeature.setRegionStart(regionGroupRegionArray[0].getRegionStart());
+                                            hearsayFeature.setRegionStop(regionGroupRegionArray[0].getRegionEnd());
+                                            hearsayFeature.setTranscriptRefSeq(transcriptRefSeq);
+                                            hearsayEM.getTransaction().begin();
+                                            hearsayDAOBean.getFeatureDAO().save(hearsayFeature);
+                                            hearsayEM.getTransaction().commit();
+                                        }
+                                    }
+                                }
+                            }
+
                         } else {
                             transcriptRefSeq = alreadyPersistedTranscriptList.get(0);
                         }
@@ -306,27 +330,6 @@ public class Scratch {
                         System.out.println(transcriptAlignment.toString());
                     } catch (HearsayDAOException e) {
                         e.printStackTrace();
-                    }
-
-                    List<Feature> canvasFeatures = canvasDAOBean.getFeatureDAO().findByRefSeqVersionAndTranscriptId(
-                            refSeqVersion, key.getVersionId());
-                    if (canvasFeatures != null && !canvasFeatures.isEmpty()) {
-                        for (Feature canvasFeature : canvasFeatures) {
-                            RegionGroup regionGroup = canvasFeature.getRegionGroup();
-                            if (regionGroup != null) {
-                                Set<RegionGroupRegion> regionGroupRegions = regionGroup.getRegionGroupRegions();
-                                if (regionGroupRegions != null && !regionGroupRegions.isEmpty()) {
-                                    RegionGroupRegion[] regionGroupRegionArray = regionGroupRegions
-                                            .toArray(new RegionGroupRegion[regionGroupRegions.size()]);
-                                    org.renci.hearsay.dao.model.Feature hearsayFeature = new org.renci.hearsay.dao.model.Feature();
-                                    hearsayFeature.setNote(canvasFeature.getNote());
-                                    hearsayFeature.setRegionStart(regionGroupRegionArray[0].getRegionStart());
-                                    hearsayFeature.setRegionStop(regionGroupRegionArray[0].getRegionEnd());
-                                    hearsayFeature.setTranscriptAlignment(transcriptAlignment);
-                                    hearsayDAOBean.getFeatureDAO().save(hearsayFeature);
-                                }
-                            }
-                        }
                     }
 
                     for (Region region : mapping.getRegions()) {
