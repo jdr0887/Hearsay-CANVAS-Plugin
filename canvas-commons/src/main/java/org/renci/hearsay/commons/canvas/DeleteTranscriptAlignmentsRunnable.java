9dcc032738fd00616bb7f902f3f2d2517375b41c
Jason Reilly <jdr0887@gmail.com>
2014-12-08 08:58:16 -0500

    initial commit

---
+++ canvas-commons/src/main/java/org/renci/hearsay/commons/canvas/DeleteTranscriptAlignmentsRunnable.java
@@ -0,0 +1,54 @@
+package org.renci.hearsay.commons.canvas;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.renci.hearsay.dao.HearsayDAOBean;
+import org.renci.hearsay.dao.HearsayDAOException;
+import org.renci.hearsay.dao.model.TranscriptAlignment;
+import org.renci.hearsay.dao.model.TranscriptRefSeq;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DeleteTranscriptAlignmentsRunnable implements Runnable {
+
+    private final Logger logger = LoggerFactory.getLogger(DeleteTranscriptAlignmentsRunnable.class);
+
+    private HearsayDAOBean hearsayDAOBean;
+
+    public DeleteTranscriptAlignmentsRunnable() {
+        super();
+    }
+
+    @Override
+    public void run() {
+        logger.debug("ENTERING call()");
+
+        try {
+            List<TranscriptRefSeq> transcriptRefSeqs = hearsayDAOBean.getTranscriptRefSeqDAO().findAll();
+            if (transcriptRefSeqs != null && !transcriptRefSeqs.isEmpty()) {
+                for (TranscriptRefSeq transcriptRefSeq : transcriptRefSeqs) {
+                    Set<TranscriptAlignment> alignmentSet = transcriptRefSeq.getAlignments();
+                    if (alignmentSet != null && !alignmentSet.isEmpty()) {
+                        List<TranscriptAlignment> alignments = new ArrayList<TranscriptAlignment>();
+                        alignments.addAll(alignmentSet);
+                        hearsayDAOBean.getTranscriptAlignmentDAO().delete(alignments);
+                    }
+                }
+            }
+        } catch (HearsayDAOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public HearsayDAOBean getHearsayDAOBean() {
+        return hearsayDAOBean;
+    }
+
+    public void setHearsayDAOBean(HearsayDAOBean hearsayDAOBean) {
+        this.hearsayDAOBean = hearsayDAOBean;
+    }
+
+}
