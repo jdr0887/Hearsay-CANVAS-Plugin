3a243fbf6119ca0598bc99504644c7cd12996d0c
Jason Reilly <jdr0887@gmail.com>
2015-01-21 08:21:11 -0500

    version is a String

--- canvas-dao/canvas-dao-api/src/main/java/org/renci/hearsay/canvas/exac/dao/VariantFrequencyDAO.java
+++ canvas-dao/canvas-dao-api/src/main/java/org/renci/hearsay/canvas/exac/dao/VariantFrequencyDAO.java
@@ -8,7 +8,7 @@ import org.renci.hearsay.dao.HearsayDAOException;
 
 public interface VariantFrequencyDAO extends BaseDAO<VariantFrequency, Long> {
 
-    public List<VariantFrequency> findByLocationVariantIdAndVersionAndPopulation(Long locVarId, Integer version,
+    public List<VariantFrequency> findByLocationVariantIdAndVersionAndPopulation(Long locVarId, String version,
             String population) throws HearsayDAOException;
 
 }
