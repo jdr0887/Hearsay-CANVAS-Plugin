794306890d9359e2732f012bfefe5a919999ce7c
Jason Reilly <jdr0887@gmail.com>
2015-02-19 13:12:58 -0500

    adding geneName

--- canvas-commands/src/main/java/org/renci/hearsay/commands/canvas/PullFeaturesAction.java
+++ canvas-commands/src/main/java/org/renci/hearsay/commands/canvas/PullFeaturesAction.java
@@ -3,8 +3,9 @@ package org.renci.hearsay.commands.canvas;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import org.apache.karaf.shell.commands.Argument;
+import org.apache.commons.lang.StringUtils;
 import org.apache.karaf.shell.commands.Command;
+import org.apache.karaf.shell.commands.Option;
 import org.apache.karaf.shell.console.AbstractAction;
 import org.renci.hearsay.canvas.dao.CANVASDAOBean;
 import org.renci.hearsay.commons.canvas.PullFeaturesCallable;
@@ -17,8 +18,11 @@ public class PullFeaturesAction extends AbstractAction {
 
     private final Logger logger = LoggerFactory.getLogger(PullFeaturesAction.class);
 
-    @Argument(index = 0, name = "refSeqVersion", description = "RefSeq Version Identifier", required = true, multiValued = false)
+    @Option(name = "--refSeqVersion", description = "RefSeq Version Identifier", required = true, multiValued = false)
     private String refSeqVersion;
+
+    @Option(name = "--geneName", description = "RefSeq Version Identifier", required = false, multiValued = false)
+    private String geneName;
 
     private CANVASDAOBean canvasDAOBean;
 
@@ -32,6 +36,9 @@ public class PullFeaturesAction extends AbstractAction {
     public Object doExecute() {
         logger.debug("ENTERING doExecute()");
         PullFeaturesCallable callable = new PullFeaturesCallable(canvasDAOBean, hearsayDAOBean, refSeqVersion);
+        if (StringUtils.isNotEmpty(geneName)) {
+            callable.setGeneName(geneName);
+        }
         try {
             ExecutorService es = Executors.newSingleThreadExecutor();
             es.submit(callable);
@@ -40,6 +47,14 @@ public class PullFeaturesAction extends AbstractAction {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public String getGeneName() {
+        return geneName;
+    }
+
+    public void setGeneName(String geneName) {
+        this.geneName = geneName;
     }
 
     public String getRefSeqVersion() {
