7a6bb13704a1e9e6d874fe76a5743eb9452415c3
Jason Reilly <jdr0887@gmail.com>
2015-11-10 15:19:52 -0500

    commenting out impl

--- canvas-commands/src/main/java/org/renci/hearsay/commands/canvas/PersistTranscriptsCallable.java
+++ canvas-commands/src/main/java/org/renci/hearsay/commands/canvas/PersistTranscriptsCallable.java
@@ -5,8 +5,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
-import javax.swing.GroupLayout.Alignment;
-
 import org.renci.hearsay.canvas.dao.CANVASDAOBean;
 import org.renci.hearsay.canvas.dao.model.Mapping;
 import org.renci.hearsay.canvas.refseq.dao.model.RefSeqCodingSequence;
@@ -15,6 +13,7 @@ import org.renci.hearsay.canvas.refseq.dao.model.RegionGroup;
 import org.renci.hearsay.canvas.refseq.dao.model.RegionGroupRegion;
 import org.renci.hearsay.dao.HearsayDAOBean;
 import org.renci.hearsay.dao.HearsayDAOException;
+import org.renci.hearsay.dao.model.Alignment;
 import org.renci.hearsay.dao.model.Gene;
 import org.renci.hearsay.dao.model.ReferenceSequence;
 import org.renci.hearsay.dao.model.Region;
@@ -85,88 +84,88 @@ public class PersistTranscriptsCallable implements Runnable {
         }
         logger.info(transcriptRefSeq.toString());
 
-        TreeSet<Region> regions = mapping.getRegions();
-
-        Alignment alignment = new Alignment();
-
-        alignment.setReferenceSequence(transcriptRefSeq);
-        alignment.setStrandType(mapping.getStrandType());
-        alignment.setGenomicStart(regions.first().toRange().getMinimumInteger());
-        alignment.setGenomicStop(regions.last().toRange().getMaximumInteger());
-
-        List<RefSeqCodingSequence> refSeqCodingSequenceResults = null;
-        try {
-            refSeqCodingSequenceResults = canvasDAOBean.getRefSeqCodingSequenceDAO()
-                    .findByRefSeqVersionAndTranscriptId(refSeqVersion, versionId);
-        } catch (HearsayDAOException e) {
-            e.printStackTrace();
-        }
-
-        if (refSeqCodingSequenceResults == null
-                || (refSeqCodingSequenceResults != null && refSeqCodingSequenceResults.isEmpty())) {
-            Iterator<Region> exonsAscendingIter = regions.iterator();
-            while (exonsAscendingIter.hasNext()) {
-                Region exon = exonsAscendingIter.next();
-                exon.setRegionType(org.renci.hearsay.dao.model.RegionType.UTR);
-            }
-        }
-
-        if (refSeqCodingSequenceResults != null && !refSeqCodingSequenceResults.isEmpty()) {
-            RefSeqCodingSequence refSeqCDS = refSeqCodingSequenceResults.get(0);
-            alignment.setProtein(refSeqCDS.getProteinId());
-            Set<RegionGroup> regionGroupSet = refSeqCDS.getLocations();
-            RegionGroup[] regionGroupArray = regionGroupSet.toArray(new RegionGroup[regionGroupSet.size()]);
-            RegionGroup regionGroup = regionGroupArray[0];
-            Set<RegionGroupRegion> regionGroupRegionSet = regionGroup.getRegionGroupRegions();
-
-            RegionGroupRegion[] regionGroupRegionArray = regionGroupRegionSet
-                    .toArray(new RegionGroupRegion[regionGroupRegionSet.size()]);
-
-            RegionGroupRegion rgr = regionGroupRegionArray[0];
-            Integer regionStart = rgr.getRegionStart();
-            Integer regionEnd = regionGroupRegionArray[regionGroupRegionSet.size() - 1].getRegionEnd();
-            alignment.setProteinRegionStart(regionStart);
-            alignment.setProteinRegionStop(regionEnd);
-            TranscriptUtil.addUTR5s(mapping, regionStart);
-            TranscriptUtil.addUTR3s(mapping, regionEnd);
-            TranscriptUtil.addCDSCoordinates(mapping, regionStart);
-        }
-        TranscriptUtil.addIntrons(mapping);
-
-        try {
-            Long mappedTranscriptId = hearsayDAOBean.getAlignmentDAO().save(alignment);
-            alignment.setId(mappedTranscriptId);
-            logger.info(alignment.toString());
-        } catch (HearsayDAOException e) {
-            e.printStackTrace();
-        }
-
-        for (Region region : mapping.getRegions()) {
-            org.renci.hearsay.dao.model.Region hearsayRegion = new org.renci.hearsay.dao.model.Region();
-            hearsayRegion.setAlignment(alignment);
-            hearsayRegion.setRegionType(region.getRegionType());
-            hearsayRegion.setRegionStart(region.getGenomeStart());
-            hearsayRegion.setRegionStop(region.getGenomeStop());
-            hearsayRegion.setTranscriptStart(region.getTranscriptStart());
-            hearsayRegion.setTranscriptStop(region.getTranscriptStop());
-            hearsayRegion.setCdsStart(region.getContigStart());
-            hearsayRegion.setCdsStop(region.getContigStop());
-
-            logger.info(region.toString());
-
-            try {
-                Long id = hearsayDAOBean.getRegionDAO().save(hearsayRegion);
-                hearsayRegion.setId(id);
-            } catch (HearsayDAOException e) {
-                e.printStackTrace();
-            }
-            alignment.getRegions().add(hearsayRegion);
-        }
-        try {
-            hearsayDAOBean.getAlignmentDAO().save(alignment);
-        } catch (HearsayDAOException e) {
-            e.printStackTrace();
-        }
+//        TreeSet<Region> regions = mapping.getRegions();
+//
+//        Alignment alignment = new Alignment();
+//
+//        alignment.getReferenceSequences().add(transcriptRefSeq);
+//        
+//        alignment.setGenomicStart(regions.first().toRange().getMinimumInteger());
+//        alignment.setGenomicStop(regions.last().toRange().getMaximumInteger());
+//
+//        List<RefSeqCodingSequence> refSeqCodingSequenceResults = null;
+//        try {
+//            refSeqCodingSequenceResults = canvasDAOBean.getRefSeqCodingSequenceDAO()
+//                    .findByRefSeqVersionAndTranscriptId(refSeqVersion, versionId);
+//        } catch (HearsayDAOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        if (refSeqCodingSequenceResults == null
+//                || (refSeqCodingSequenceResults != null && refSeqCodingSequenceResults.isEmpty())) {
+//            Iterator<Region> exonsAscendingIter = regions.iterator();
+//            while (exonsAscendingIter.hasNext()) {
+//                Region exon = exonsAscendingIter.next();
+//                exon.setRegionType(org.renci.hearsay.dao.model.RegionType.UTR);
+//            }
+//        }
+//
+//        if (refSeqCodingSequenceResults != null && !refSeqCodingSequenceResults.isEmpty()) {
+//            RefSeqCodingSequence refSeqCDS = refSeqCodingSequenceResults.get(0);
+//            alignment.setProtein(refSeqCDS.getProteinId());
+//            Set<RegionGroup> regionGroupSet = refSeqCDS.getLocations();
+//            RegionGroup[] regionGroupArray = regionGroupSet.toArray(new RegionGroup[regionGroupSet.size()]);
+//            RegionGroup regionGroup = regionGroupArray[0];
+//            Set<RegionGroupRegion> regionGroupRegionSet = regionGroup.getRegionGroupRegions();
+//
+//            RegionGroupRegion[] regionGroupRegionArray = regionGroupRegionSet
+//                    .toArray(new RegionGroupRegion[regionGroupRegionSet.size()]);
+//
+//            RegionGroupRegion rgr = regionGroupRegionArray[0];
+//            Integer regionStart = rgr.getRegionStart();
+//            Integer regionEnd = regionGroupRegionArray[regionGroupRegionSet.size() - 1].getRegionEnd();
+//            alignment.setProteinRegionStart(regionStart);
+//            alignment.setProteinRegionStop(regionEnd);
+//            TranscriptUtil.addUTR5s(mapping, regionStart);
+//            TranscriptUtil.addUTR3s(mapping, regionEnd);
+//            TranscriptUtil.addCDSCoordinates(mapping, regionStart);
+//        }
+//        TranscriptUtil.addIntrons(mapping);
+//
+//        try {
+//            Long mappedTranscriptId = hearsayDAOBean.getAlignmentDAO().save(alignment);
+//            alignment.setId(mappedTranscriptId);
+//            logger.info(alignment.toString());
+//        } catch (HearsayDAOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        for (Region region : mapping.getRegions()) {
+//            org.renci.hearsay.dao.model.Region hearsayRegion = new org.renci.hearsay.dao.model.Region();
+//            hearsayRegion.setAlignment(alignment);
+//            hearsayRegion.setRegionType(region.getRegionType());
+//            hearsayRegion.setRegionStart(region.getGenomeStart());
+//            hearsayRegion.setRegionStop(region.getGenomeStop());
+//            hearsayRegion.setTranscriptStart(region.getTranscriptStart());
+//            hearsayRegion.setTranscriptStop(region.getTranscriptStop());
+//            hearsayRegion.setCdsStart(region.getContigStart());
+//            hearsayRegion.setCdsStop(region.getContigStop());
+//
+//            logger.info(region.toString());
+//
+//            try {
+//                Long id = hearsayDAOBean.getRegionDAO().save(hearsayRegion);
+//                hearsayRegion.setId(id);
+//            } catch (HearsayDAOException e) {
+//                e.printStackTrace();
+//            }
+//            alignment.getRegions().add(hearsayRegion);
+//        }
+//        try {
+//            hearsayDAOBean.getAlignmentDAO().save(alignment);
+//        } catch (HearsayDAOException e) {
+//            e.printStackTrace();
+//        }
 
     }
 
