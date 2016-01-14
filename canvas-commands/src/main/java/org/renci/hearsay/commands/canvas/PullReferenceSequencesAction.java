bf853413ca8ef0ef5a93a9eedc57544f981f348c
Jason Reilly <jdr0887@gmail.com>
2016-01-14 16:00:15 -0500

    fixing package name

---
+++ canvas-commands/src/main/java/org/renci/hearsay/commands/canvas/PullReferenceSequencesAction.java
@@ -0,0 +1,69 @@
+package org.renci.hearsay.commands.canvas;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.apache.karaf.shell.api.action.Action;
+import org.apache.karaf.shell.api.action.Argument;
+import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.lifecycle.Reference;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
+import org.renci.hearsay.canvas.dao.CANVASDAOBeanService;
+import org.renci.hearsay.dao.HearsayDAOBeanService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Command(scope = "canvas", name = "pull-reference-sequences", description = "Pull ReferenceSequences")
+@Service
+public class PullReferenceSequencesAction implements Action {
+
+    private static final Logger logger = LoggerFactory.getLogger(PullReferenceSequencesAction.class);
+
+    @Argument(index = 0, name = "refSeqVersion", description = "RefSeq Version Identifier", required = true, multiValued = false)
+    private String refSeqVersion;
+
+    @Argument(index = 1, name = "genomeRefId", description = "GenomeRef Identifier", required = true, multiValued = false)
+    private Integer genomeRefId;
+
+    @Reference
+    private CANVASDAOBeanService canvasDAOBeanService;
+
+    @Reference
+    private HearsayDAOBeanService hearsayDAOBeanService;
+
+    public PullReferenceSequencesAction() {
+        super();
+    }
+
+    @Override
+    public Object execute() {
+        logger.debug("ENTERING execute()");
+        try {
+            logger.info("genomeRefId = {}", genomeRefId);
+            logger.info("refSeqVersion = {}", refSeqVersion);
+            ExecutorService es = Executors.newSingleThreadExecutor();
+            es.submit(new PullReferenceSequencesRunnable(canvasDAOBeanService, hearsayDAOBeanService, refSeqVersion, genomeRefId));
+            es.shutdown();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public String getRefSeqVersion() {
+        return refSeqVersion;
+    }
+
+    public void setRefSeqVersion(String refSeqVersion) {
+        this.refSeqVersion = refSeqVersion;
+    }
+
+    public Integer getGenomeRefId() {
+        return genomeRefId;
+    }
+
+    public void setGenomeRefId(Integer genomeRefId) {
+        this.genomeRefId = genomeRefId;
+    }
+
+}
