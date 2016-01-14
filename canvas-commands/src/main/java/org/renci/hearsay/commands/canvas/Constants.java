148dbda15c94dddcc89e1a23ca4f26d9bdcfa97e
Jason Reilly <jdr0887@gmail.com>
2016-01-14 15:56:57 -0500

    initial commit

---
+++ canvas-commands/src/main/java/org/renci/hearsay/commands/canvas/Constants.java
@@ -0,0 +1,15 @@
+package org.renci.hearsay.commands.canvas;
+
+public interface Constants {
+
+    public static final String REF_GENOME_REF_SEQ_VERSION_ACCESSION = "canvas/ref/genomeRefSeq/verAccession";
+
+    public static final String REFSEQ_CDS_PROTEIN_ID = "canvas/refseq/cds/proteinId";
+
+    public static final String REFSEQ_TRANSCRIPT_VERSION_ID = "canvas/refseq/transcript/versionId";
+
+    public static final String REFSEQ_GENE_ID = "canvas/refseq/gene/id";
+
+    public static final String REF_GENOME_REF_ID = "canvas/ref/genomeRef/id";
+
+}
