91988c673f97bbfe74c6c9d6f7ed1e41ce38615b
Jason Reilly <jdr0887@gmail.com>
2016-01-14 15:58:24 -0500

    using lambdas

---
+++ canvas-commands/src/main/java/org/renci/hearsay/commands/canvas/PullFeaturesCallable.java
@@ -0,0 +1,177 @@
+package org.renci.hearsay.commands.canvas;
+
+import static org.renci.hearsay.commands.canvas.Constants.REFSEQ_TRANSCRIPT_VERSION_ID;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.renci.hearsay.canvas.dao.CANVASDAOBeanService;
+import org.renci.hearsay.canvas.refseq.dao.model.Feature;
+import org.renci.hearsay.canvas.refseq.dao.model.RegionGroup;
+import org.renci.hearsay.canvas.refseq.dao.model.RegionGroupRegion;
+import org.renci.hearsay.dao.HearsayDAOBeanService;
+import org.renci.hearsay.dao.HearsayDAOException;
+import org.renci.hearsay.dao.model.Gene;
+import org.renci.hearsay.dao.model.Identifier;
+import org.renci.hearsay.dao.model.Location;
+import org.renci.hearsay.dao.model.ReferenceSequence;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PullFeaturesCallable implements Callable<Void> {
+
+    private static final Logger logger = LoggerFactory.getLogger(PullFeaturesCallable.class);
+
+    private CANVASDAOBeanService canvasDAOBeanService;
+
+    private HearsayDAOBeanService hearsayDAOBeanService;
+
+    private String refSeqVersion;
+
+    private String geneName;
+
+    public PullFeaturesCallable(CANVASDAOBeanService canvasDAOBeanService, HearsayDAOBeanService hearsayDAOBeanService,
+            String refSeqVersion) {
+        super();
+        this.canvasDAOBeanService = canvasDAOBeanService;
+        this.hearsayDAOBeanService = hearsayDAOBeanService;
+        this.refSeqVersion = refSeqVersion;
+    }
+
+    @Override
+    public Void call() throws HearsayDAOException {
+        logger.debug("ENTERING call()");
+
+        List<ReferenceSequence> referenceSequences = new ArrayList<ReferenceSequence>();
+        if (StringUtils.isNotEmpty(geneName)) {
+            List<Gene> geneList = hearsayDAOBeanService.getGeneDAO().findBySymbol(geneName);
+            if (CollectionUtils.isNotEmpty(geneList)) {
+                referenceSequences.addAll(hearsayDAOBeanService.getReferenceSequenceDAO().findByGeneId(geneList.get(0).getId()));
+            }
+        } else {
+            referenceSequences.addAll(hearsayDAOBeanService.getReferenceSequenceDAO().findByIdentifierSystem(REFSEQ_TRANSCRIPT_VERSION_ID));
+        }
+
+        if (CollectionUtils.isNotEmpty(referenceSequences)) {
+            ExecutorService es = Executors.newFixedThreadPool(4);
+
+            for (ReferenceSequence refseq : referenceSequences) {
+
+                es.submit(() -> {
+
+                    try {
+                        logger.info(refseq.toString());
+
+                        String accession = null;
+                        for (Identifier identifier : refseq.getIdentifiers()) {
+                            if (identifier.getSystem().equals(REFSEQ_TRANSCRIPT_VERSION_ID)) {
+                                accession = identifier.getValue();
+                            }
+                        }
+                        logger.info(accession);
+
+                        List<Feature> canvasFeatures = canvasDAOBeanService.getFeatureDAO()
+                                .findByRefSeqVersionAndTranscriptId(refSeqVersion, accession);
+                        if (CollectionUtils.isNotEmpty(canvasFeatures)) {
+                            for (Feature canvasFeature : canvasFeatures) {
+                                logger.info(canvasFeature.toString());
+                                RegionGroup regionGroup = canvasFeature.getRegionGroup();
+                                if (regionGroup != null) {
+                                    Set<RegionGroupRegion> regionGroupRegions = regionGroup.getRegionGroupRegions();
+                                    if (CollectionUtils.isNotEmpty(regionGroupRegions)) {
+
+                                        RegionGroupRegion[] regionGroupRegionArray = regionGroupRegions
+                                                .toArray(new RegionGroupRegion[regionGroupRegions.size()]);
+
+                                        org.renci.hearsay.dao.model.Feature hearsayFeature = new org.renci.hearsay.dao.model.Feature();
+                                        if (StringUtils.isNotEmpty(canvasFeature.getNote())) {
+                                            hearsayFeature.setNote(canvasFeature.getNote());
+                                        }
+
+                                        Location regionLocation = new Location(regionGroupRegionArray[0].getKey().getRegionStart(),
+                                                regionGroupRegionArray[0].getKey().getRegionEnd());
+                                        regionLocation.setId(hearsayDAOBeanService.getLocationDAO().save(regionLocation));
+                                        hearsayFeature.getLocations().add(regionLocation);
+                                        hearsayFeature.setId(hearsayDAOBeanService.getFeatureDAO().save(hearsayFeature));
+                                        logger.info(hearsayFeature.toString());
+
+                                        refseq.getFeatures().add(hearsayFeature);
+                                    }
+                                }
+                            }
+                        }
+
+                        hearsayDAOBeanService.getReferenceSequenceDAO().save(refseq);
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                });
+
+            }
+            es.shutdown();
+        }
+        return null;
+    }
+
+    public String getGeneName() {
+        return geneName;
+    }
+
+    public void setGeneName(String geneName) {
+        this.geneName = geneName;
+    }
+
+    public String getRefSeqVersion() {
+        return refSeqVersion;
+    }
+
+    public void setRefSeqVersion(String refSeqVersion) {
+        this.refSeqVersion = refSeqVersion;
+    }
+
+    class PersistFeaturesCallable implements Callable<Void> {
+
+        private final Logger logger = LoggerFactory.getLogger(PersistFeaturesCallable.class);
+
+        private ReferenceSequence transcriptRefSeq;
+
+        private String refSeqVersion;
+
+        public PersistFeaturesCallable() {
+            super();
+        }
+
+        @Override
+        public Void call() throws HearsayDAOException {
+            logger.debug("ENTERING call()");
+
+            return null;
+        }
+
+        public ReferenceSequence getTranscriptRefSeq() {
+            return transcriptRefSeq;
+        }
+
+        public void setTranscriptRefSeq(ReferenceSequence transcriptRefSeq) {
+            this.transcriptRefSeq = transcriptRefSeq;
+        }
+
+        public String getRefSeqVersion() {
+            return refSeqVersion;
+        }
+
+        public void setRefSeqVersion(String refSeqVersion) {
+            this.refSeqVersion = refSeqVersion;
+        }
+
+    }
+
+}
