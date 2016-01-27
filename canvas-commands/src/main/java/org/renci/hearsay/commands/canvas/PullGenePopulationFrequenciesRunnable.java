8f10ffdfe484d367e7964f2337cfc12d9878e07b
Jason Reilly <jdr0887@gmail.com>
2016-01-27 10:42:54 -0500

    using convenient constructor for PopulationFrequency

---
+++ canvas-commands/src/main/java/org/renci/hearsay/commands/canvas/PullGenePopulationFrequenciesRunnable.java
@@ -0,0 +1,126 @@
+package org.renci.hearsay.commands.canvas;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.renci.hearsay.canvas.clinbin.dao.model.MaxFreq;
+import org.renci.hearsay.canvas.dao.CANVASDAOBeanService;
+import org.renci.hearsay.canvas.exac.dao.model.MaxVariantFrequency;
+import org.renci.hearsay.canvas.refseq.dao.model.Variants_61_2;
+import org.renci.hearsay.canvas.var.dao.model.LocationVariant;
+import org.renci.hearsay.dao.HearsayDAOBeanService;
+import org.renci.hearsay.dao.model.Gene;
+import org.renci.hearsay.dao.model.Location;
+import org.renci.hearsay.dao.model.PopulationFrequency;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PullGenePopulationFrequenciesRunnable implements Runnable {
+
+    private static final Logger logger = LoggerFactory.getLogger(PullGenePopulationFrequenciesRunnable.class);
+
+    private CANVASDAOBeanService canvasDAOBeanService;
+
+    private HearsayDAOBeanService hearsayDAOBeanService;
+
+    public PullGenePopulationFrequenciesRunnable(CANVASDAOBeanService canvasDAOBeanService, HearsayDAOBeanService hearsayDAOBeanService) {
+        super();
+        this.canvasDAOBeanService = canvasDAOBeanService;
+        this.hearsayDAOBeanService = hearsayDAOBeanService;
+    }
+
+    @Override
+    public void run() {
+        logger.debug("ENTERING call()");
+
+        try {
+
+            List<Gene> geneList = hearsayDAOBeanService.getGeneDAO().findAll();
+
+            ExecutorService es = Executors.newFixedThreadPool(8);
+            if (CollectionUtils.isNotEmpty(geneList)) {
+                for (Gene gene : geneList) {
+
+                    es.submit(() -> {
+
+                        logger.info(gene.toString());
+
+                        try {
+
+                            List<Variants_61_2> variants = canvasDAOBeanService.getVariants_61_2_DAO().findByGeneName(gene.getSymbol());
+
+                            if (CollectionUtils.isNotEmpty(variants)) {
+
+                                logger.info("variants.size(): {}", variants.size());
+
+                                for (final Variants_61_2 variant : variants) {
+
+                                    LocationVariant locationVariant = variant.getLocationVariant();
+
+                                    if (locationVariant != null) {
+
+                                        // logger.debug(locationVariant.toString());
+
+                                        List<MaxFreq> clinbinMaxVariantFrequencies = locationVariant.getMaxFreqs();
+                                        if (CollectionUtils.isNotEmpty(clinbinMaxVariantFrequencies)) {
+                                            for (final MaxFreq maxFreq : clinbinMaxVariantFrequencies) {
+                                                logger.debug(maxFreq.toString());
+                                                Location location = new Location(locationVariant.getPosition(),
+                                                        locationVariant.getEndPosition());
+                                                location.setId(hearsayDAOBeanService.getLocationDAO().save(location));
+                                                PopulationFrequency populationFrequency = new PopulationFrequency(
+                                                        maxFreq.getMaxAlleleFreq(), "CLINBIN",
+                                                        maxFreq.getKey().getGen1000Version().toString());
+                                                populationFrequency.setPosition(location);
+                                                populationFrequency.setGene(gene);
+                                                populationFrequency
+                                                        .setId(hearsayDAOBeanService.getPopulationFrequencyDAO().save(populationFrequency));
+                                                logger.info(populationFrequency.toString());
+                                            }
+                                        }
+
+                                        List<MaxVariantFrequency> exacMaxVariantFrequencies = locationVariant.getMaxVariantFrequencies();
+                                        if (CollectionUtils.isNotEmpty(exacMaxVariantFrequencies)) {
+                                            for (final MaxVariantFrequency maxFreq : exacMaxVariantFrequencies) {
+                                                logger.debug(maxFreq.toString());
+                                                Location location = new Location(locationVariant.getPosition(),
+                                                        locationVariant.getEndPosition());
+                                                location.setId(hearsayDAOBeanService.getLocationDAO().save(location));
+                                                PopulationFrequency populationFrequency = new PopulationFrequency(
+                                                        maxFreq.getMaxAlleleFrequency(), "EXAC", maxFreq.getKey().getVersion());
+                                                populationFrequency.setPosition(location);
+                                                populationFrequency.setGene(gene);
+                                                populationFrequency
+                                                        .setId(hearsayDAOBeanService.getPopulationFrequencyDAO().save(populationFrequency));
+                                                logger.info(populationFrequency.toString());
+                                            }
+                                        }
+
+                                    }
+
+                                }
+
+                            }
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                    });
+
+                }
+            }
+            es.shutdown();
+            es.awaitTermination(4L, TimeUnit.HOURS);
+
+        } catch (Exception e) {
+            logger.error("Error", e);
+            e.printStackTrace();
+        }
+
+    }
+
+}
